e6039b798c7528fdadb0491624cb0175f9a74c24
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-12-14 12:40:21 +0530

    Palindrome Checker done

--- Week2_PalindromeChecker.java
+++ Week2_PalindromeChecker.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class Week2_PalindromeChecker {
     
-    
+    public static boolean checkPalindrome(String word){
+        
+        word = word.toLowerCase().replaceAll("//w", " ");
+
+        int left = 0;
+        int right = word.length()-1;
+
+        while(left < right){
+            if(word.charAt(left) != word.charAt(right)){
+                return false;
+        }else{
+            left++;
+            right--;
+        }
+    }
+        return true;
+    }
     
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the word: ");
+        String word = scanner.nextLine();
+        scanner.close();
+
+        checkPalindrome(word);
+
+        if(checkPalindrome(word) == false){
+            System.out.println(word + " is not a Palindrome");
+        }else{
+            System.out.println(word + " is a Palindrome");
+        }
+
     }
 }
